ad8b0f356878a8a37a3a3aafd421d50e2cc4e055
adanbae <mostbae@naver.com>
2019-06-13 23:45:26 +0900

    simple table material v3.9 add

--- src/components/layouts/MainMenu.js
+++ src/components/layouts/MainMenu.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 // Material-ui 컴포넌트들
-import { withStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
+import { withStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
 // 아이콘들
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import SettingsIcon from '@material-ui/icons/Settings';
-import TableIcon from '@material-ui/icons/TableChart';
-import ChartIcon from '@material-ui/icons/ShowChart';
-import PagesIcon from '@material-ui/icons/Pages';
-import IconsIcon from '@material-ui/icons/Mood';
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import SettingsIcon from "@material-ui/icons/Settings";
+import TableIcon from "@material-ui/icons/TableChart";
+import ChartIcon from "@material-ui/icons/ShowChart";
+import PagesIcon from "@material-ui/icons/Pages";
+import IconsIcon from "@material-ui/icons/Mood";
 // others
-import LogoImage from '../../assets/images/logo.png';
+import LogoImage from "../../assets/images/logo.png";
 
 const styles = theme => ({
   toolbar: {
@@ -25,69 +25,93 @@ const styles = theme => ({
     background: "#536dfe",
     padding: "16px 30px",
     color: "#ffffff",
-    fontSize: "1.2em",
+    fontSize: "1.2em"
   },
   logoimg: {
     width: "32px",
     height: "32px",
-    verticalAlign: "middle",
+    verticalAlign: "middle"
   },
   mainmenuBox: { padding: "25px 0" },
   mainmenuWrap: { padding: "15px 34px 15px 30px" },
-  mainmenu: { color: "#ffffff" },
+  mainmenu: { color: "#ffffff" }
 });
 
 class ResponsiveDrawer extends React.Component {
   render() {
     const { classes } = this.props;
-    
+
     return (
       <div>
         <NavLink to="/" activeClassName="selected">
-          <div className={ classes.toolbar }>
-            <img src={LogoImage} alt="Logo" className={ classes.logoimg }/>
+          <div className={classes.toolbar}>
+            <img src={LogoImage} alt="Logo" className={classes.logoimg} />
             &nbsp; Incheon Dev's
           </div>
         </NavLink>
         <Divider />
-        <List className={ classes.mainmenuBox } >
+        <List className={classes.mainmenuBox}>
           <NavLink to="/" activeClassName="selected">
-            <ListItem className={ classes.mainmenuWrap } button>            
-                <ListItemIcon><DashboardIcon className={ classes.mainmenu }/></ListItemIcon>
-                <ListItemText><Typography className={ classes.mainmenu }>대시보드</Typography></ListItemText>
+            <ListItem className={classes.mainmenuWrap} button>
+              <ListItemIcon>
+                <DashboardIcon className={classes.mainmenu} />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography className={classes.mainmenu}>대시보드</Typography>
+              </ListItemText>
+            </ListItem>
+          </NavLink>
+          <NavLink to="/tables" activeClassName="selected">
+            <ListItem className={classes.mainmenuWrap} button>
+              <ListItemIcon>
+                <TableIcon className={classes.mainmenu} />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography className={classes.mainmenu}>테이블</Typography>
+              </ListItemText>
             </ListItem>
           </NavLink>
           <NavLink to="/" activeClassName="selected">
-            <ListItem className={ classes.mainmenuWrap } button>
-              <ListItemIcon><TableIcon className={ classes.mainmenu } /></ListItemIcon>
-              <ListItemText><Typography className={ classes.mainmenu }>테이블</Typography></ListItemText>
+            <ListItem className={classes.mainmenuWrap} button>
+              <ListItemIcon>
+                <ChartIcon className={classes.mainmenu} />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography className={classes.mainmenu}>그래프</Typography>
+              </ListItemText>
             </ListItem>
           </NavLink>
           <NavLink to="/" activeClassName="selected">
-            <ListItem className={ classes.mainmenuWrap } button>
-              <ListItemIcon><ChartIcon className={ classes.mainmenu } /></ListItemIcon>
-              <ListItemText><Typography className={ classes.mainmenu }>그래프</Typography></ListItemText>
-            </ListItem>
-          </NavLink>
-          <NavLink to="/" activeClassName="selected">
-            <ListItem className={ classes.mainmenuWrap } button>
-              <ListItemIcon><PagesIcon className={ classes.mainmenu } /></ListItemIcon>
-              <ListItemText><Typography className={ classes.mainmenu }>페이지</Typography></ListItemText>
+            <ListItem className={classes.mainmenuWrap} button>
+              <ListItemIcon>
+                <PagesIcon className={classes.mainmenu} />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography className={classes.mainmenu}>페이지</Typography>
+              </ListItemText>
             </ListItem>
           </NavLink>
         </List>
         <Divider />
-        <List className={ classes.mainmenuBox } >
+        <List className={classes.mainmenuBox}>
           <NavLink to="/icons" activeClassName="selected">
-            <ListItem className={ classes.mainmenuWrap } button>
-              <ListItemIcon><IconsIcon className={ classes.mainmenu } /></ListItemIcon>
-              <ListItemText><Typography className={ classes.mainmenu }>아이콘들</Typography></ListItemText>
+            <ListItem className={classes.mainmenuWrap} button>
+              <ListItemIcon>
+                <IconsIcon className={classes.mainmenu} />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography className={classes.mainmenu}>아이콘들</Typography>
+              </ListItemText>
             </ListItem>
           </NavLink>
           <NavLink to="/" activeClassName="selected">
-            <ListItem className={ classes.mainmenuWrap } button>
-              <ListItemIcon><SettingsIcon className={ classes.mainmenu } /></ListItemIcon>
-              <ListItemText><Typography className={ classes.mainmenu }>기타</Typography></ListItemText>
+            <ListItem className={classes.mainmenuWrap} button>
+              <ListItemIcon>
+                <SettingsIcon className={classes.mainmenu} />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography className={classes.mainmenu}>기타</Typography>
+              </ListItemText>
             </ListItem>
           </NavLink>
         </List>
@@ -98,7 +122,7 @@ class ResponsiveDrawer extends React.Component {
 
 ResponsiveDrawer.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
 };
 
 export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);
--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,50 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import { Route, Switch } from "react-router-dom";
 // Material-ui 컴포넌트들
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { withStyles } from "@material-ui/core/styles";
 // others
-import Header from './Header.js';
-import AsideNav from './AsideNav.js';
-import HomePage from '../pages/Home.js';
-import IconsPage from '../pages/Icons.js';
+import Header from "./Header.js";
+import AsideNav from "./AsideNav.js";
+import HomePage from "../pages/Home.js";
+import IconsPage from "../pages/Icons.js";
+import TablesPage from "../pages/Tables.js";
 
 const styles = theme => ({
-  root: { display: 'flex' },
+  root: { display: "flex" },
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-  },
+    padding: theme.spacing.unit * 3
+  }
 });
 
 const Layouts = props => {
   const { classes } = props;
-  
+
   return (
-    <div className={ classes.root }>
+    <div className={classes.root}>
       <CssBaseline />
-      <Header onDrawerToggle={ props.onDrawerToggle } />
-      <AsideNav 
-        mobileOpen={ props.mobileOpen }
-        onDrawerToggle={ props.onDrawerToggle }
+      <Header onDrawerToggle={props.onDrawerToggle} />
+      <AsideNav
+        mobileOpen={props.mobileOpen}
+        onDrawerToggle={props.onDrawerToggle}
       />
 
-      <main className={ classes.content }>
-        <div className={ classes.toolbar } />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Switch>
-          <Route exact path="/" component={ HomePage } />
-          <Route exact path="/icons" component={ IconsPage } />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/icons" component={IconsPage} />
+          <Route exact path="/tables" component={TablesPage} />
         </Switch>
       </main>
     </div>
   );
-}
+};
 
 Layouts.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
   // container: PropTypes.object,
 };
 
